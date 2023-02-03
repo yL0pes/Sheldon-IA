a2f2ae79afbf8e598d873981d3899a8c0d014df8
yL0pes <lopeshenrique538@gmail.com>
2023-02-03 17:04:18 -0300

    add vosk

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
-# Principal 
+#!/usr/bin/env python3
 
-import speech_recognition as sr 
+from vosk import Model, KaldiRecognizer
+import os
+import pyaudio
 
-    #Cria um reconhecedor
-r = sr.Recognizer()
+if not os.path.exists("model"):
+     print ("Please download the model from https://alphacephei.com/vask/models and unpack as 'model' in the current folder.")
+     exit (1)
+ 
+model = Model("model")
+rec = KaldiRecognizer(model, 16000)
 
+p = pyaudio.PyAudio()
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000)
+stream.start_stream()  
 
-#Abrir dispositivo de audio
+while True:
+    data = stream.read(4000)
+    if len(data) == 0:
+         break
+    if rec.AcceptWaveform(data):
+        print(rec.Result())
+    else:
+         print(rec.PartialResult())
 
-with sr.Microphone() as source:
-  while True:
-      audio = r.listen(source) #Define microfone como fonte de áudio
-
-      print(r.recognize_google(audio, language='pt'))
+print(rec.FinalResult())
